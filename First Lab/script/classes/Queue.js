f9f974273299e54ab02c13051f9c02ec3aa68b38
Yehor Makarenko <mak.yehor.knu@gmail.com>
2023-03-07 18:42:15 +0200

    Removed private fields

--- First Lab/script/classes/Queue.js
+++ First Lab/script/classes/Queue.js
@@ -1,50 +1,50 @@
 export default class Queue {  
-  #value = null; 
-  #next = null;
-  #head = null;
-  #tale = null;
-  #length = 0;
+  value = null; 
+  next = null;
+  head = null;
+  tale = null;
+  length = 0;
 
   get length() {
-    return this.#length;
+    return this.length;
   }  
 
   push(value) {
     const newItem = new Queue;
-    newItem.#value = value;
-    this.#length++;
+    newItem.value = value;
+    this.length++;
 
-    if (this.#head === null) {
-      this.#head = newItem;
-      this.#tale = newItem;
+    if (this.head === null) {
+      this.head = newItem;
+      this.tale = newItem;
       return;
     }
 
-    this.#tale.#next = newItem;
-    this.#tale = newItem;
+    this.tale.next = newItem;
+    this.tale = newItem;
   }
 
   shift() {
-    if (this.#head === null) return null;
+    if (this.head === null) return null;
     
-    const resValue = this.#head.#value;
-    this.#length--;
-    this.#head = this.#head.#next;
+    const resValue = this.head.value;
+    this.length--;
+    this.head = this.head.next;
     return resValue;
   }
 
   get() {
-    if (this.#head === null) return null;
-    return this.#head.#value;
+    if (this.head === null) return null;
+    return this.head.value;
   }
 
   static copy(queue) {
     const copy = new Queue();
-    let currItem = queue.#head;
-
+    let currItem = queue.head;
+  
     while (currItem !== null) {
-      copy.push(currItem.#value);
-      currItem = currItem.#next;
+      copy.push(currItem.value);
+      currItem = currItem.next;
     }
 
     return copy;
@@ -55,11 +55,11 @@ export default class Queue {
     let currItem;
 
     for (let queue of queues) {
-      currItem = queue.#head;
+      currItem = queue.head;
 
       while (currItem !== null) {
-        resultQueue.push(currItem.#value);
-        currItem = currItem.#next;
+        resultQueue.push(currItem.value);
+        currItem = currItem.next;
       }
     }
 
--- First Lab/script/classes/Stack.js
+++ First Lab/script/classes/Stack.js
@@ -1,34 +1,34 @@
 export default class Stack {
-  #value = null;
-  #prev = null;
-  #tale = null;
-  #length = 0;
+  value = null;
+  prev = null;
+  tale = null;
+  length = 0;
 
   get length() {
-    return this.#length;
+    return this.length;
   }
 
   push(value) {
     const newItem = new Stack();
 
-    newItem.#value = value;
-    newItem.#prev = this.#tale;
-    this.#tale = newItem;
-    this.#length++;
+    newItem.value = value;
+    newItem.prev = this.tale;
+    this.tale = newItem;
+    this.length++;
   }
 
   pop() {
-    if (this.#tale === null) return null;
+    if (this.tale === null) return null;
 
-    const resValue = this.#tale.#value;
-    this.#tale = this.#tale.#prev;
-    this.#length--;
+    const resValue = this.tale.value;
+    this.tale = this.tale.prev;
+    this.length--;
 
     return resValue;
   }
 
   get() {
-    if (this.#tale === null) return null;
-    return this.#tale.#value;
+    if (this.tale === null) return null;
+    return this.tale.value;
   }
 }
